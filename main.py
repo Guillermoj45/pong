a6cb49e45d9073a8adcfab610a96b4c7a5aa896f
guill <guillermoremesa@gmail.com>
2025-02-04 00:12:03 +0100

    inicio

--- main.py
+++ main.py
@@ -1,22 +1,26 @@
 import pygame, sys
-from pygame.examples.go_over_there import screen, clock
 
-# Initialize Pygame
+# Inicialización de Pygame
+pygame.init()
 
+# Variables de la pantalla
 screen_width = 1280
-screen_height = 860
+screen_height = 760
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("Pong")
+clock = pygame.time.Clock()  # Define el reloj localmente
 
-# Game Variables
-
+# Variables del juego
 ball = pygame.Rect(screen_width/2 - 15, screen_height/2 - 15, 30, 30)
 # ball2 = pygame.Rect(screen_width/2 - 15, screen_height/2 - 15, 30, 30)
 player = pygame.Rect(10, screen_height/2 - 70, 10, 140)
-opponent = pygame.Rect(10, screen_height/2 - 70, 10, 140)
+opponent = pygame.Rect(screen_width - 20, screen_height/2 - 70, 10, 140)  # Posición en el lado derecho
 
 bg_color = pygame.Color('grey12')
 light_grey = (200, 200, 200)
+
+ball_speed_x = 7
+ball_speed_y = 7
 
 
 while True:
@@ -25,14 +29,23 @@ while True:
             pygame.quit()
             sys.exit()
 
-    #Visuals
+    ball.x += ball_speed_x
+    ball.y += ball_speed_y
+
+    if ball.top <= 0 or ball.bottom >= screen_height:
+        ball_speed_y *= -1
+    if ball.left <= 0 or ball.right >= screen_width:
+        ball_speed_x *= -1
+
+    if ball.contains(player) or ball.colliderect(opponent):
+        ball_speed_x *= -1
+
+    # Visuales
     screen.fill(bg_color)
     pygame.draw.rect(screen, light_grey, player)
     pygame.draw.rect(screen, light_grey, opponent)
     pygame.draw.ellipse(screen, light_grey, ball)
-    # pygame.draw.ellipse(screen, light_grey, ball)
     pygame.draw.aaline(screen, light_grey, (screen_width/2, 0), (screen_width/2, screen_height))
 
     pygame.display.flip()
     clock.tick(60)
-
